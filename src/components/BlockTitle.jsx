b304382400180d427838d943624881c3d3ec003a
HenriqueNeubert <neubert.study@gmail.com>
2022-11-03 20:11:40 -0300

    feat: added color in title border

--- src/components/BlockTitle.jsx
+++ src/components/BlockTitle.jsx
@@ -6,11 +6,15 @@ export default props =>{
     justifyContent: props.align
   }
 
+  const border = {
+    borderColor: props.border
+  }
+
   return(
     <div>
       <div className="container">
         <div className="d-flex" style={align}>
-          <h3 className="fw-bold title--block">
+          <h3 className="fw-bold title--block" style={border}>
             {props.title}
           </h3>
         </div>
--- src/components/technologies/Technologies.jsx
+++ src/components/technologies/Technologies.jsx
@@ -7,7 +7,7 @@ export default (props) => {
   return (
     <section class="technologies py-5 z-index-2 bgGradient">
       <div className="container">
-        <BlockTitle title="TÉCNOLOGIAS" align="start" />
+        <BlockTitle title="TÉCNOLOGIAS" align="start" border="white" />
         <div className="technologiesCarroussel d-flex justify-content-center row row-cols-3">
           <TechnologiesItem></TechnologiesItem>
         </div>
